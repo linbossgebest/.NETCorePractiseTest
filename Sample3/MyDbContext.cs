d59eca393bd00d3113cb3c7d9413c1b4ce04ce57
linbossgebest <809621397@qq.com>
2021-04-20 18:02:47 +0800

    add sample03 with mysql efcore

---
+++ Sample3/MyDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Sample3.Model;
+using System.IO;
+
+namespace Sample3
+{
+    public class MyDbContext:DbContext
+    {
+        public DbSet<Content> Content { get; set; }
+
+        private IConfiguration configuration;
+
+        public MyDbContext()
+        {
+            configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                                      .AddJsonFile("appsettings.json")
+                                                      .Build();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(configuration.GetConnectionString("Default"));
+        }
+    }
+}
---
+++ Sample3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbcontext = new MyDbContext())
+            {
+                dbcontext.Content.Add(new Model.Content() 
+                {
+                    title="efcore title1",
+                    content="efcore content1"
+                });
+
+                dbcontext.SaveChanges();
+
+                foreach (var item in dbcontext.Content)
+                {
+                    Console.WriteLine($"{item.id}-{item.title}-{item.content}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
